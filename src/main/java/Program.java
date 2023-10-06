7bf43e0b0ffca708f80956ed4ce0a382ef772c32
Olga Komar <komar_ol@mail.ru>
2023-10-06 06:32:52 +0300

    added solution to tasks 2 and 4

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -3,43 +3,49 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Program {
-    public static void main(String[] args) throws Exception {
-       // printFloatNum();
+    public static void main(String[] args)  {
+
+        /**
+         * задание 1
+         */
+        printFloatNum();
 
         /**
          * задание 2
          */
-//        try {
-//            int d = 0;
-//            double catchedRes1 = intArray[8] / d;
-//            System.out.println("catchedRes1 = " + catchedRes1);
-//        } catch (ArithmeticException e) {
-//            System.out.println("Catching exception: " + e);
-//        }
+        int[] intArray = {1,2,3,4,5,6,7,8,9};
+        try {
+            int d = 0;
+            double catchedRes1 = intArray[8] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        } catch (ArithmeticException e) {
+            System.out.println("Catching exception: " + e);
+        }
 
         /**
          * задание 3
          */
-//        try {
-//            int a = 90;
-//            int b = 3;
-//            System.out.println(a / b);
-//            printSum(23, 234);
-//            int[] abc = {1, 2};
-//            abc[3] = 9;
-//        } catch (Throwable ex) {
-//            System.out.println("Что-то пошло не так...");
-//        } catch (NullPointerException ex) {
-//            System.out.println("Указатель не может указывать на null!");
-//        } catch (IndexOutOfBoundsException ex) {
-//            System.out.println("Массив выходит за пределы своего размера!");
-//        }
+        try {
+            int a = 90;
+            int b = 3;
+            System.out.println(a / b);
+            printSum(23, 234);
+            int[] abc = {1, 2};
+            abc[3] = 9;
+        } catch (NullPointerException ex) {
+            System.out.println("Указатель не может указывать на null!");
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("Массив выходит за пределы своего размера!");
+        } catch (ArithmeticException ex){
+            System.out.println("Деление на ноль не допустимо");
+        }
 
         /**
          * задание 4
          */
 
         isEmptyStr();
+
     }
 
     /**
@@ -50,7 +56,7 @@ public class Program {
         try {
             System.out.println("Введите дробное число: ");
             float num = scanner.nextFloat();
-            System.out.printf("Введенное вами число %.2f", num);
+            System.out.printf("Введенное вами число %.2f\n", num);
         } catch (InputMismatchException e) {
             System.out.println("Что-то пошло не так, давайте попробуем снова");
             printFloatNum();
@@ -62,7 +68,7 @@ public class Program {
     /**
      * задание 3
      */
-    public static void printSum(Integer a, Integer b) throws FileNotFoundException {
+    public static void printSum(int a, int b) {
         System.out.println(a + b);
     }
 
